bc1979e7d70d98195a04ecb507cc6d5b3d3969fc
Avi Tuchband <avitoohband@gmail.com>
2023-10-18 14:22:32 +0300

    scenes, redux state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Home } from "@mui/icons-material";
+import { BrouwerRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
-function App() {
-  return (
-    <div className="app">
+function App() {  
+  return <div className="app">
+    <BrouwerRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/profile/:userId" element={<ProfilePage />} />
+    </Routes>
 
-    </div>
-  );
+    </BrouwerRouter>
+  </div>;
 }
-
 export default App;
