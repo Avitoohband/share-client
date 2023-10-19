18d36f9da1e5076398035a968afca0f385291546
Avi Tuchband <avitoohband@gmail.com>
2023-10-19 10:27:02 +0300

    dark mode

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import { BrouwerRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
+import state from "state";
 
-function App() {  
-  return <div className="app">
-    <BrouwerRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/profile/:userId" element={<ProfilePage />} />
-    </Routes>
+function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => {
+    createTheme(themeSettings(mode), [mode]);
+  });
 
-    </BrouwerRouter>
-  </div>;
+  return (
+    <div className="app">
+      <BrouwerRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrouwerRouter>
+    </div>
+  );
 }
 export default App;
