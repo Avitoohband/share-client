dd97b771eb8aac0bba612162d89171067655d11f
Avi Tuchband <avitoohband@gmail.com>
2023-11-03 19:31:35 +0200

    my post widget addons

--- src/scenes/widgets/MyPostWidgets.jsx
+++ src/scenes/widgets/MyPostWidgets.jsx
@@ -23,10 +23,70 @@ import UserImage from "components/UserImage";
 import WidgetWrapper from "components/WidgetWrapper";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setPost } from "state";
+import { setPost, setPosts } from "state";
 
 const MyPostWidget = () => {
-    // 04:05
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [postContent, setPostContent] = useState("");
+  const { palette } = useTheme();
+  const { _id: id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", id);
+    formData.append("description", postContent);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    }
+
+    const response = await fetch("http://localhost/3001/posts", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap=" 1.5rem">
+        <UserImage image={picturePath} />
+        <InputBase
+          placeholder="What's on your mind..."
+          onChange={(e) => setPostContent(e.target.value)}
+          value={postContent}
+          sx={{
+            width: "100%",
+            backgroundColor: palette.neutral.light,
+            borderRadius: "2rem",
+            padding: "1rem 2rem",
+          }}
+        />
+      </FlexBetween>
+      {isImage && (
+        <Box
+          border={`1px solid ${medium}`}
+          borderRadius="5px"
+          mt="1rem"
+          p="1rem"
+        >
+          {/* Dropzone 04:17 */}
+        </Box>
+      )}
+    </WidgetWrapper>
+  );
 };
 
 export default MyPostWidget;
